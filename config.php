ec8827664555df2bcfcfc3a0b9accbb510814d30
GlobalVentures.com <maidabarrientos@gmail.com>
2023-05-13 15:39:46 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = [
+    'servername' => 'localhost',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'dbname' => 'your_database_name',
+    'openai_key' => 'your_openai_api_key',
+    'domain' => 'your_domain_name'
+];
